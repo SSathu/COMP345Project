2fd898637915a02f269749155a5389af7e5347f9
Arerrerra <147214497+Arerrerra@users.noreply.github.com>
2023-10-06 22:51:45 -0400

    Update MainDriver.cpp

--- A1/MainDriver.cpp
+++ A1/MainDriver.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "MapDriver.h"
+#include "CardDriver.h"
+#include "PlayerDriver.h"
+#include "OrdersDriver.h"
+#include "GameEngineDriver.h"
 
+
+int main() {
+  cout << "Map: " << endl;
+  testLoadMaps();
+  cout << "Player: " << endl;
+	testPlayers();
+  cout << "Order: " << endl;
+  testOrdersLists();
+  cout << "Card: " << endl;
+  testCards();
+  cout << "Game engine: " << endl;
+  testGameStates();
+	return 0;
+
+}
