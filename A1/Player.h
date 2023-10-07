f94c923e109bf9ad7623f13f7d085cfdab1d1345
Arerrerra <147214497+Arerrerra@users.noreply.github.com>
2023-10-06 22:12:55 -0400

    Player

---
+++ A1/Player.h
@@ -0,0 +1,53 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class PlayerOrder {
+public:
+	// Constructor
+	PlayerOrder(string playerOrder);
+	// Get order method
+	string getOrder();
+
+private:
+	// store order
+	string playerOrder;
+};
+
+class Player {
+public:
+	// Constuctors and Destructor
+	Player();
+	Player(string name, vector<string> territory, vector<string> card, vector<PlayerOrder*> orderList);
+	Player(Player &player);
+	~Player();
+
+	// assignment operator
+	Player& operator = (Player& player);
+	// stream insertion operator
+	friend ostream& operator << (ostream& os, Player& player);
+	// Show a list of territories to be defended
+	void toDefend();
+	// Show a list of territories to be attacked
+	void toAttack();
+	// Put orders in a list
+	void issueOrder(string order);
+	
+	
+private:
+	// Store name of a player
+	string name;
+	// Store territories
+	vector<string> territory;
+	// Store cards
+	vector<string> card;
+	// Store a list of orders
+	vector<PlayerOrder*> orderList;
+};
+
+
+#endif;
