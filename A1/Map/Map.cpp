04a7813d056e0ab7270042b434565c207a11eb24
Kiran Freund <114677393+5DollarCostcoChicken@users.noreply.github.com>
2023-10-26 00:21:27 -0400

    Update Map.cpp

--- A1/Map/Map.cpp
+++ A1/Map/Map.cpp
@@ -7,23 +7,22 @@ using namespace std;
 
 //Territory Constructors
 Territory::Territory(string name, string cont, int x, int y) {
-	playerOwner = &name;
-	continent = &cont;
-	int xx = x;
-	xCoord = &xx;
-	int yy = y;
-	yCoord = &yy;
-	bool visit = false;
+    territoryName = new std::string(name);
+	continent = new std::string(cont);;
+	xCoord = new int(x);
+	yCoord = new int(y);
+	visited = new bool(false);
+    numArmies = new int(0);
 	neighbors = new vector<Territory*>;
 }
 Territory::Territory(Territory* copy) {
-	playerOwner = new std::string(copy->getName());
+    territoryName = new std::string(copy->getName());
+    playerOwner = new std::string(copy->getPlayer());
 	continent = new std::string(copy->getContinent());
-	int xx = copy->getXCoord();
-	xCoord = &xx;
-	int yy = copy->getYCoord();
-	yCoord = &yy;
-	bool visit = false;
+	xCoord = new int(copy->getXCoord());
+	yCoord = new int(copy->getYCoord());
+    visited = new bool(false);
+    numArmies = new int(0);
 	neighbors = copy->neighbors;
 }
 
@@ -50,7 +49,7 @@ void Territory::setPlayer(string* newName) {
 	playerOwner = newName;
 }
 string Territory::getName() const {
-	return *territoryName;
+    return *territoryName;
 }
 string Territory::getContinent() const {
 	return *continent;
@@ -68,7 +67,7 @@ void Territory::addNeighbor(Territory* neighbor) {
 	neighbors->push_back(neighbor);
 }
 
-void Territory::deleteVariables() {
+Territory::~Territory() {
 	//deallocates all variables to not have memory leaks or dangling pointers
 	delete playerOwner;
 	playerOwner = nullptr;
@@ -78,20 +77,24 @@ void Territory::deleteVariables() {
 	continent = nullptr;
 	delete xCoord;
 	xCoord = nullptr;
-	delete yCoord;
-	yCoord = nullptr;
-	delete numArmies;
-	numArmies = nullptr;
+    delete yCoord;
+    yCoord = nullptr;
+    delete numArmies;
+    numArmies = nullptr;
 	delete neighbors;
 	neighbors = nullptr;
+    delete visited;
+    visited = nullptr;
 }
 
 //Map constructors
 Map::Map() {
 	territories = new vector<Territory*>;
 }
-Map::Map(Map* copy) {
-	territories = copy->territories;
+Map::Map(const Map& copy)
+{
+    // Perform a deep copy of the territories vector
+    territories = new vector<Territory*>(*copy.territories);
 }
 
 //stream insertion operator for Map
@@ -106,15 +109,15 @@ void Map::streamInsertion() {
 
 //Map validate()
 bool Map::validate() {
-	//Check #1, making sure all territories are connected
+    //Check #1, making sure all territories are connected
 	if (!isConnected())
 		return false;
-
 	//Check #2, making sure all continents are connected
 	map<string, Map> continentGraphs;
 	// Sort the original graph and sort nodes into new graphs.
+
 	for (Territory* node : *territories) {
-		// Check if a graph for this continent already exists.
+        // Check if a graph for this continent already exists.
 		if (continentGraphs.find(node->getContinent()) == continentGraphs.end()) {
 			// Create a new graph for this continent.
 			continentGraphs[node->getContinent()] = Map();
@@ -129,7 +132,6 @@ bool Map::validate() {
 		if (!map.isConnected())
 			return false;
 	}
-
 	//Check #3, making sure all territories are part of only 1 continent
 	for (Territory* var : *territories)
 	{
@@ -139,10 +141,9 @@ bool Map::validate() {
 	return true;
 }
 bool Map::isConnected() {
-	if (territories->empty()) {
-		return true;
+    if (territories->empty()) {
+            return false;
 	}
-
 	stack<Territory*> stack;
 	stack.push((*territories)[0]);
 
@@ -159,7 +160,7 @@ bool Map::isConnected() {
 	}
 
 	for (Territory* node : *territories) {
-		if (!node->visited) {
+        if (!node->visited) {
 			return false;
 		}
 	}
@@ -172,27 +173,25 @@ bool MapLoader::createMapFromFile(string& fileName, Map* map2make) {
 	ifstream inputFileStream;
 	inputFileStream.open(fileName);
 
-	//Reject if file couldnt open
+	//Reject if file couldn't open
 	if (!inputFileStream.is_open()) {
 		cout << "Error: Could not open file." << endl;
 		return false; // Return an error code
 	}
-	//If the first line of the file isnt "[Map]", reject file
+	//If the first line of the file isn't "[Map]", reject file
 	string nextLine;
 	if (!getline(inputFileStream, nextLine) || nextLine != "[Map]") {
-		inputFileStream.close();
-		return false;
-	}
-
+        inputFileStream.close();
+        return false;
+    }
 	//Skip all other lines until we reach "[Continents]"
 	//If this line isn't reached, reject file
 	while (nextLine != "[Continents]") {
-		if (!getline(inputFileStream, nextLine)) {
-			inputFileStream.close();
-			return false;
-		}
-	}
-
+        if (!getline(inputFileStream, nextLine)) {
+            inputFileStream.close();
+            return false;
+        }
+    }
 	//Read Continents until we reach an empty line (or i guess the "[Territories]" line)
 	//link all continents to a map<string, int>, with continent name being identifier and territory amount being data
 	//we'll use these to verify validity of map later
@@ -203,43 +202,44 @@ bool MapLoader::createMapFromFile(string& fileName, Map* map2make) {
 			inputFileStream.close();
 			return false;
 		}
-		try { //successfully indexed continent 
-			int index = nextLine.find("=");
-			continents.insert(make_pair(nextLine.substr(0, index), stoi(nextLine.substr(index))));
+		try { //successfully indexed continent
+            if (!nextLine.empty() && nextLine != "[Territories]") {
+                int index = nextLine.find("=");
+                //remove all spaces from the continent name
+                std::string substring = nextLine.substr(0, index);
+                substring.erase(std::remove(substring.begin(), substring.end(), ' '), substring.end());
+                //add the continent into the list
+                continents.insert(make_pair(substring, stoi(nextLine.substr(index + 1))));
+            }
 		}
 		catch (const std::exception& e) {
-			inputFileStream.close();
+            inputFileStream.close();
 			return false;
 		}
 	}
 
 	//reach "[Territories]" line, and begin loop until end of file
 	//if no territories in map, reject
-	//be creating another map that temporarily stores all the Territories by their names
-	Map outputMap = new Map();
 	map<string, Territory*> tempTerritorylist;
 	vector<vector<string>> neighborList;
 	while (!inputFileStream.eof()) {
 		//First pass: assign territories their name, x coord, y coord, and continent name
 		//skip over empty lines, those don't matter
-		if (getline(inputFileStream, nextLine) && !nextLine.empty()) {
-			try {
-				//remove spaces
+        if (getline(inputFileStream, nextLine) && !nextLine.empty()) {
+            try {
+                //remove spaces
 				nextLine.erase(std::remove_if(nextLine.begin(), nextLine.end(), ::isspace), nextLine.end());
-
-				//split string by commas
+                //split string by commas
 				vector<string> nextLineElements;
 				size_t start = 0, end;
-
 				while ((end = nextLine.find(',', start)) != string::npos) {
 					nextLineElements.push_back(nextLine.substr(start, end - start));
 					start = end + 1;
 				}
 				nextLineElements.push_back(nextLine.substr(start));
-
 				//create a territory from the initial elements, and add it to the map list
 				Territory* newTerritory = new Territory(nextLineElements[0], nextLineElements[3], stoi(nextLineElements[1]), stoi(nextLineElements[2]));
-				outputMap.territories->push_back(newTerritory);
+                map2make->territories->push_back(newTerritory);
 				tempTerritorylist.insert(make_pair(newTerritory->getName(), newTerritory));
 
 				//If continent number is now negative, or continent cannot be found, reject file
@@ -252,6 +252,7 @@ bool MapLoader::createMapFromFile(string& fileName, Map* map2make) {
 				//add the remaining list, which should be the neighbors, to the neighbor list to be saved
 				nextLineElements.erase(nextLineElements.begin(), nextLineElements.begin() + 4);
 				neighborList.push_back(nextLineElements);
+
 			}
 			catch (const std::exception& e) {
 				inputFileStream.close();
@@ -259,14 +260,13 @@ bool MapLoader::createMapFromFile(string& fileName, Map* map2make) {
 			}
 		}
 	}
-
 	//Second pass: assign territories their neighbors
 	//if a territory has no neighbors, reject file (not fully connected)
-	if (outputMap.territories->size() == neighborList.size() && outputMap.territories->size() > 0) {
+	if (map2make->territories->size() == neighborList.size() && map2make->territories->size() > 0) {
 		for (int i = 0; i < neighborList.size(); i++) {
 			if (neighborList[i].size() > 0) {
 				for (const string& neighbor : neighborList[i]) {
-					outputMap.territories->at(i)->addNeighbor(tempTerritorylist[neighbor]);
+                    map2make->territories->at(i)->addNeighbor(tempTerritorylist[neighbor]);
 				}
 			}
 			else {
@@ -282,6 +282,7 @@ bool MapLoader::createMapFromFile(string& fileName, Map* map2make) {
 
 	//if we haven't returned false yet, return the map.
 	inputFileStream.close();
-	map2make = new Map(outputMap);
+
+
 	return true;
 }
