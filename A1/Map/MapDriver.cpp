3e1f5141c6da28d60e882ab03c1686e599836c2b
Kiran Freund <114677393+5DollarCostcoChicken@users.noreply.github.com>
2023-10-26 00:19:09 -0400

    Update MapDriver.cpp

--- A1/Map/MapDriver.cpp
+++ A1/Map/MapDriver.cpp
@@ -6,14 +6,19 @@
 
 using namespace std;
 
+int main(){
+    MapDriver::testLoadMaps();
+    return 0;
+}
+
 void MapDriver::testLoadMaps() {
 	MapLoader mapDriver = MapLoader();
 
-	string mapFile1 = "../MapFiles/Texas";
-	string mapFile2 = "../MapFiles/Skyland";
-	string mapFile3 = "../MapFiles/Roman_Empire";
+	string mapFile1 = "../MapFiles/Texas.map";
+	string mapFile2 = "../MapFiles/Roman Empire.map";
 
-	Map* mapTest1;
+	Map* mapTest1 = new Map();
+#pragma region Test1
 	bool test1 = mapDriver.createMapFromFile(mapFile1, mapTest1);
 	if (test1) {
 		cout << "Successful creation of a map from " << mapFile1 << endl;
@@ -27,4 +32,26 @@ void MapDriver::testLoadMaps() {
 	else {
 		cout << "Map from " << mapFile1 << " is invalid!" << endl;
 	}
+    mapTest1->territories->clear();
+    delete mapTest1;
+#pragma endregion
+
+    Map* mapTest2 = new Map();
+#pragma region Test2
+    bool test2 = mapDriver.createMapFromFile(mapFile2, mapTest2);
+    if (test2) {
+        cout << "Successful creation of a map from " << mapFile2 << endl;
+    }
+    else {
+        cout << "Unsuccessful creation of a map from " << mapFile2 << endl;
+    }
+    if (mapTest2->validate()) {
+        cout << "Map from " << mapFile2 << " is valid!" << endl;
+    }
+    else {
+        cout << "Map from " << mapFile2 << " is invalid!" << endl;
+    }
+    mapTest2->territories->clear();
+    delete mapTest2;
+#pragma endregion
 }
